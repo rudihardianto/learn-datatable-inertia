cf25d29c031be3c2d267d0da23aade4b2f7be881
unknown <rud.hardianto@gmail.com>
2022-03-02 06:55:46 +0700

    setup user table & seed

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,31 +6,33 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
-    }
+   /**
+    * Run the migrations.
+    *
+    * @return void
+    */
+   public function up()
+   {
+      Schema::create('users', function (Blueprint $table) {
+         $table->id();
+         $table->string('username', 25)->unique();
+         $table->string('name');
+         $table->string('email')->unique();
+         $table->timestamp('email_verified_at')->nullable();
+         $table->string('password');
+         $table->text('address');
+         $table->rememberToken();
+         $table->timestamps();
+      });
+   }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('users');
-    }
+   /**
+    * Reverse the migrations.
+    *
+    * @return void
+    */
+   public function down()
+   {
+      Schema::dropIfExists('users');
+   }
 };
