fab54eff7e4267393a9a020b2b067d6dd149d27b
unknown <rud.hardianto@gmail.com>
2022-03-02 07:21:49 +0700

    resource & collection

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\UserResource;
 use App\Models\User;
 
 class UserController extends Controller
 {
    public function index()
    {
-      return User::paginate();
+      return UserResource::collection(User::paginate());
    }
 }
---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+   /**
+    * Transform the resource into an array.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+    */
+   public function toArray($request)
+   {
+      return [
+         'id'         => $this->id,
+         'username'   => $this->username,
+         'name'       => $this->name,
+         'email'      => $this->email,
+         'address'    => $this->address,
+         'created_at' => $this->created_at->diffForHumans(),
+         'updated_at' => $this->updated_at->diffForHumans(),
+      ];
+   }
+}
